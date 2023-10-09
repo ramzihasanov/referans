0f01088e111a87150fd03fc5d345a9a097df681b
ramzihasanov <bd54tkjwr@code.edu.az>
2023-10-09 09:41:05 +0400

    clas car

---
+++ ConsoleApp2/Cars.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public  class Cars
+    {
+         public  string Brand;
+        public  string Model;
+        public  float CurrentFuel;
+        public  float FuelFor1km;
+        public float  Millage;
+
+           public void  Drive()
+        {
+            
+             float wontDrive;
+            float b = CurrentFuel / FuelFor1km;
+            bool a = float.TryParse(Console.ReadLine(), out wontDrive);
+            if (a)
+            {
+                if (b >= wontDrive)
+                {
+                    Millage += wontDrive;
+                    CurrentFuel = CurrentFuel - (wontDrive * FuelFor1km);
+                    Console.WriteLine( "millage : " + Millage + " " + "currentfuel: " + CurrentFuel);
+                }
+                else  Console.WriteLine("error"); ;
+            }
+            else  Console.WriteLine("error"); ;
+        }
+    }
+     
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars car=new Cars();
+            car.Brand = "Mercedes";
+            car.Model = "S Class";
+            car.CurrentFuel = 20;
+            car.FuelFor1km = 1;
+            car.Millage = 245;
+            car.Drive();
+
+
+
+        }
+    }
+}
